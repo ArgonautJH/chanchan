bccde4364542446f84fbb41db2b56ad8dbde4554
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-04-16 21:00:11 +0900

    feat: dashboard UI 위젯 이동 가능하도록 세팅

--- Start/js/dashboard.js
+++ Start/js/dashboard.js
@@ -6,6 +6,17 @@ $(function () {
 	// 	inline: true
 	// })
 
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	// Make the dashboard widgets sortable Using jquery UI
+	$('.connectedSortable').sortable({
+		placeholder: 'sort-highlight',
+		connectWith: '.connectedSortable',
+		handle: '.card-header, .nav-tabs',
+		forcePlaceholderSize: true,
+		zIndex: 999999
+	})
+	$('.connectedSortable .card-header').css('cursor', 'move')
+
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	// Chart.js Charts
 	// Sales chart
